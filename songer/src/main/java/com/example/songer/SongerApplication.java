4efad5e577857aafa1b13358ef512174b17301e4
maram-mustafa <maram.w.mustafa@gmail.com>
2021-08-10 22:10:29 +0300

    add files

---
+++ songer/src/main/java/com/example/songer/SongerApplication.java
@@ -0,0 +1,13 @@
+package com.example.songer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongerApplication.class, args);
+	}
+
+}
---
+++ songer/src/test/java/com/example/songer/SongerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.songer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
