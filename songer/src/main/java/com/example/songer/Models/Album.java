ec6fc96e6f3126404342a7cf261795457430d18b
maram-mustafa <maram.w.mustafa@gmail.com>
2021-08-12 02:11:15 +0300

    lab-12

--- songer/src/main/java/com/example/songer/Models/Album.java
+++ songer/src/main/java/com/example/songer/Models/Album.java
@@ -12,9 +12,9 @@ public class Album {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int songCount;
     private String title;
     private String artist;
-    private int songCount;
     private int length;
     private String imageUrl;
 
@@ -22,10 +22,9 @@ public class Album {
     }
 
 
-    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+    public Album(String title, String artist, int length, String imageUrl) {
         this.title = title;
         this.artist = artist;
-        this.songCount = songCount;
         this.length = length;
         this.imageUrl = imageUrl;
     }
--- songer/src/main/java/repositories/AlbumRepository.java
+++
@@ -1,6 +0,0 @@
-package repositories;
-import com.example.songer.Models.Album;
-
-
-public interface AlbumRepository extends CrudRepository <Album,Integer> {
-}
